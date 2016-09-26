6588d3a595f9bf4b150ccc0fb1b57695d82cce61
stevemasteller <stevermasteller@gmail.com>
2016-09-26 12:19:13 -0400

    Added main-description section

--- js/app.js
+++ js/app.js
@@ -58,14 +58,14 @@ var displayMovies = function (results) {
 
 $(document).on('click', 'div.poster-wrap', function() {
 	
-	searchTitle = $(this).next('.movie-title').text();
-	searchYear  = $(this).next('.movie-title').next('.movie-year').text();
+	thisSearchTitle = $(this).next('.movie-title').text();
+	thisSearchYear  = $(this).next('.movie-title').next('.movie-year').text();
 	
-	alert(searchTitle + searchYear);
+	alert(thisSearchTitle + thisSearchYear);
 	
 	omdbOptions = {
-		t: searchTitle,
-		y: searchYear,
+		t: thisSearchTitle,
+		y: thisSearchYear,
 		plot: "full",
 		r: "json"
 	};
@@ -75,6 +75,44 @@ $(document).on('click', 'div.poster-wrap', function() {
 
 var displayDescription = function (results) {
 	
+	var movieHTML = '';
+	
 	$('div.main-content').hide();
 	$('div.main-description').show();
+	
+	// Grey Bar HTML
+	movieHTML += '<div id="grey-bar">';
+	movieHTML +=   '<div class="inner2">';
+    movieHTML +=     '<div id="index-link">';
+	movieHTML +=       '<a href="index.html"><  </a><span>Search results</span>';
+	movieHTML +=     '</div>';
+    movieHTML +=     '<div class="inner3">';
+	movieHTML +=       '<h1 class="movie-title">' + results.Title + ' (' + results.Year + ')</h1>';
+	movieHTML +=       '<span class="IMDB-rating">IMDB Rating: ' + results.imdbRating + '</span>';
+	movieHTML +=     '</div>';
+	movieHTML +=   '</div>';
+	movieHTML += '</div>';
+
+	// Bottom Bar HTML
+	movieHTML += '<div id="bottom-bar">';
+	movieHTML +=   '<div class="inner4">';
+	movieHTML +=     '<div class="poster-wrapper">';
+	
+	if (results.Poster === 'N/A') {
+		movieHTML +=   '<i class="material-icons poster-placeholder">crop_original</i>'
+	} else {
+		movieHTML +=   '<img class="movie-poster" src="' + results.Poster + '">';
+	}
+	
+	movieHTML +=     '</div>';
+	movieHTML +=     '<div class="inner3">';
+	movieHTML +=       '<h1 class="movie-synopsis">Plot Synopsis:</h1>';
+	movieHTML +=       '<p>' + results.Plot + '</p>'
+	movieHTML +=       '<a href="http://www.imdb.com/title/' + results.imdbID + '"><button id="IMDB-button">View on IMDB</button></a>';
+	movieHTML +=     '</div>';
+	movieHTML +=   '</div>';
+	movieHTML += '</div>';
+
+	
+	$('div.main-description').html(movieHTML);
 }
