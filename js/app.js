4789574fbb87cd131d836b08990422ba3ceffffb
stevemasteller <stevermasteller@gmail.com>
2016-09-26 13:31:31 -0400

    Added comments

--- js/app.js
+++ js/app.js
@@ -1,52 +1,73 @@
+/***********************************************************
+*
+*  Movie Search
+*
+*  Searches for movies based on Title and Year. A poster, 
+*  title, and year of all matching movies are displayed.
+*  Clicking on a poster brings up another page with a 
+*  description, a link to the omdb page, and the omdb rating
+*  as well.
+*
+*  Author: Steve Masteller
+*  Email: stevermasteller@gmail.com
+*
+************************************************************/
 
-var omdbAPI = "http://www.omdbapi.com/?";
-var searchTitle;
-var searchYear;
-var thisSearchTitle;
-var thisSearchYear;
+var omdbAPI = "http://www.omdbapi.com/?";	// address to ombd API
 
+var searchTitle;		// title used for movie search
+var searchYear;			// year used for movie search
+var thisSearchTitle;	// title used to find description of movie
+var thisSearchYear;		// year used to find description of movie
+
+// Event handler for #index-link. Brings user back to the original movie search page
 $(document).on('click', 'div #index-link', function() {
 	
 	callMovieSearch();
 });
 
+// Event handler for movie search
 $('form').submit( function(event) {
-	event.preventDefault();
+	event.preventDefault();				// prevent the submit, use ajax instead
 	
-	searchTitle = $('#search').val();
+	searchTitle = $('#search').val();	
 	searchYear = $('#year').val();
 	
 	callMovieSearch();
 });
 
+// setup and call the omdb API for a movie search by title and year 
 var callMovieSearch = function () {
 	
 	omdbOptions = {
-		s: searchTitle,
-		type: "movie",
-		y: searchYear,
-		r: "json"
+		s: searchTitle,		// title to search by
+		type: "movie",		// search only for movies
+		y: searchYear,		// year to search by
+		r: "json"			// return data in json format
 	};
 
-	$.getJSON(omdbAPI, omdbOptions, displayMovies);
+	$.getJSON(omdbAPI, omdbOptions, displayMovies); // call omdb API
 };
  
-
+// Create HTML for display the results of a movie search
 var displayMovies = function (results) {
 	 
 	var moviesHTML = '';
 	
-	$('div.main-description').hide();
-	$('div.main-content').show();
+	$('div.main-description').hide();	// don't show a movie description
+	$('div.main-content').show();		// show the movie search results
 	 
+	// check for a response
 	if (results.Response === "True") {
 		 
+		// cycle throuch each movie in the json
 		$.each(results.Search, function (i, movie) {
 			
+			// creat a li for each movie in the json
 			moviesHTML += '<li>';
 			moviesHTML += '<div class="poster-wrap">';
 			
-			
+			// if there is no movie poster display a placeholder, else display movie poster
 			if (movie.Poster === 'N/A') {
 				moviesHTML += '<i class="material-icons poster-placeholder">crop_original</i>'
 			} else {
@@ -54,21 +75,23 @@ var displayMovies = function (results) {
 			}
 			
 			moviesHTML += '</div>';
-			moviesHTML += '<span class="movie-title">' + movie.Title + '</span>';
-			moviesHTML += '<span class="movie-year">' + movie.Year + '</span>';
+			moviesHTML += '<span class="movie-title">' + movie.Title + '</span>';	// movie title
+			moviesHTML += '<span class="movie-year">' + movie.Year + '</span>';		// movie year
 			moviesHTML += '</li>';
 		});
 		
+	// no results, display no movies found message.
 	} else {
-		
 		moviesHTML += "<li class='no-movies'>";
 		moviesHTML += "<i class='material-icons icon-help'>help_outline</i>No movies found that match: " + searchTitle + ".";
 		moviesHTML += "</li>"; 
 	}
 	
+	// attach moviesHTML to document in the correct place
 	$('#movies').html(moviesHTML);	 
 }
 
+// Event handler for poster clicks. When a poster is clicked get a movie description and display it.
 $(document).on('click', 'div.poster-wrap', function() {
 	
 	thisSearchTitle = $(this).next('.movie-title').text();
@@ -77,25 +100,27 @@ $(document).on('click', 'div.poster-wrap', function() {
 	callMovieDescription();
 });
 
+// setup and call the ombd API to get a movie plot and ombd rating for a particular movie and year
 var callMovieDescription = function () {
 	
 	omdbOptions = {
-		t: thisSearchTitle,
-		type: "movie",
-		y: thisSearchYear,
-		plot: "full",
-		r: "json"
+		t: thisSearchTitle,		// title movie poster clicked
+		type: "movie",			// get only movie info
+		y: thisSearchYear,		// year of movie poster clicked
+		plot: "full",			// get full plot
+		r: "json"				// get results in json format
 	};
 
-	$.getJSON(omdbAPI, omdbOptions, displayDescription);
+	$.getJSON(omdbAPI, omdbOptions, displayDescription);	// call omdb API.
 };
 
+// Display plot and other info of particular movie selected by clicking on poster in movie search screen
 var displayDescription = function (results) {
 	
 	var movieHTML = '';
 	
-	$('div.main-content').hide();
-	$('div.main-description').show();
+	$('div.main-content').hide();		// hide movie search results
+	$('div.main-description').show();	// display movie description results
 	
 	// Grey Bar HTML
 	movieHTML += '<div id="grey-bar">';
@@ -104,8 +129,8 @@ var displayDescription = function (results) {
 	movieHTML +=       '<span id="link"><  </span><span id="search-results">Search results</span>';
 	movieHTML +=     '</div>';
     movieHTML +=     '<div class="inner3">';
-	movieHTML +=       '<h1 class="movie-title">' + results.Title + ' (' + results.Year + ')</h1>';
-	movieHTML +=       '<span class="IMDB-rating">IMDB Rating: ' + results.imdbRating + '</span>';
+	movieHTML +=       '<h1 class="movie-title">' + results.Title + ' (' + results.Year + ')</h1>';  // title and year
+	movieHTML +=       '<span class="IMDB-rating">IMDB Rating: ' + results.imdbRating + '</span>';	 // imdb rating
 	movieHTML +=     '</div>';
 	movieHTML +=   '</div>';
 	movieHTML += '</div>';
@@ -115,6 +140,7 @@ var displayDescription = function (results) {
 	movieHTML +=   '<div class="inner4">';
 	movieHTML +=     '<div class="poster-wrapper">';
 	
+	// if there is no movie poster display a placeholder, else display movie poster
 	if (results.Poster === 'N/A') {
 		movieHTML +=   '<i class="material-icons poster-placeholder">crop_original</i>'
 	} else {
@@ -124,12 +150,14 @@ var displayDescription = function (results) {
 	movieHTML +=     '</div>';
 	movieHTML +=     '<div class="inner3">';
 	movieHTML +=       '<h1 class="movie-synopsis">Plot Synopsis:</h1>';
-	movieHTML +=       '<p>' + results.Plot + '</p>'
-	movieHTML +=       '<a href="http://www.imdb.com/title/' + results.imdbID + '"><button id="IMDB-button">View on IMDB</button></a>';
+	movieHTML +=       '<p>' + results.Plot + '</p>'	// display the full plot
+	// link to imdb page for a particular title
+	movieHTML +=       '<a href="http://www.imdb.com/title/' + results.imdbID + '"><button id="IMDB-button">View on IMDB</button></a>';	
 	movieHTML +=     '</div>';
 	movieHTML +=   '</div>';
 	movieHTML += '</div>';
 
 	
+	// attach movieHTML to document in the correct place
 	$('div.main-description').html(movieHTML);
 }
